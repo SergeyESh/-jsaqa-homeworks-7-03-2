03d0d409e43529589476359c64e60718e9531224
SergeyESh <SergeyESh1989@yandex.ru>
2023-04-11 16:40:24 +0300

    Задача 2 полностью выполнена

---
+++ tests/userAuthorization.test.js
@@ -0,0 +1,22 @@
+const { test, expect } = require("@playwright/test");
+
+const user = require("../user");
+
+test("successful authorization", async ({ page }) => {
+     
+    await page.goto("https://netology.ru/?modal=sign_in");
+    await page.getByPlaceholder("Email").fill(user.validUser.login);
+    await page.getByPlaceholder("Пароль").fill(user.validUser.pass);  
+    await page.getByTestId("login-submit-btn").click();
+    await expect(page).toHaveTitle("Мои программы обучения", {timeout: 30000});
+
+});
+
+test("unsuccessful authorization", async ({ page }) => {
+     
+    await page.goto("https://netology.ru/?modal=sign_in");
+    await page.getByPlaceholder("Email").fill(user.invalidUser.login);
+    await page.getByPlaceholder("Пароль").fill(user.invalidUser.pass);  
+    await page.getByTestId("login-submit-btn").click();
+    await expect(page.getByTestId("login-error-hint")).toHaveText("Вы ввели неправильно логин или пароль");
+});
